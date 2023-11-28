26034f4e8dd94840eadb5370ee34d31e9c606a8e
Arydiane <arydianejardim@gmail.com>
2023-11-28 17:48:49 -0300

    adiciona BaseComponent

---
+++ src/theme/BaseComponent.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import { StyleSheet } from "@src/theme/StyleSheet";
+import { parseStyleSheet } from '@displaykit/responsive_styles';
+
+interface StyledBaseComponent {
+  styleSheet?: StyleSheet; 
+}
+
+const StyledBaseComponent = styled.div<StyledBaseComponent>`
+  ${({ styleSheet }) => parseStyleSheet(styleSheet)}
+`
+
+export const BaseComponent = (props) => {
+  return (
+    <StyledBaseComponent {...props} />
+  )
+}
+
+BaseComponent.defaultProps = {
+  styleSheet: {},
+}
