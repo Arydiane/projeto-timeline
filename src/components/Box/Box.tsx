4fb85820c972dbd0496e18869a3665121345509f
Arydiane <arydianejardim@gmail.com>
2023-11-28 22:02:34 -0300

    atualiza componente  Box aceita novas tags

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -2,9 +2,9 @@ import { BaseComponent } from "@src/theme/BaseComponent";
 import { StyleSheet } from "@src/theme/StyleSheet";
 
 interface BoxProps {
-  children: React.ReactNode; 
-  styleSheet: StyleSheet; 
-  tag: any;
+  children?: React.ReactNode; 
+  styleSheet?: StyleSheet; 
+  tag?: 'main' | 'div' | 'article' | 'section' | 'ul' | string;
 }
 
 export default function Box({ children, styleSheet , tag, ...props} : BoxProps) {
