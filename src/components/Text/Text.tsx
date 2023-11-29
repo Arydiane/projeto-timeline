4b53726776a6a61795763ea3e312fa4d165b731a
Arydiane <arydianejardim@gmail.com>
2023-11-28 22:21:20 -0300

    atualiza componente Text adiciona useTheme

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,16 +1,18 @@
 import { StyleSheet } from "@src/theme/StyleSheet";
-import theme from "@src/theme/theme";
+import { ThemeTypographyVariants } from "@src/theme/theme";
 import { BaseComponent } from "@src/theme/BaseComponent";
+import { useTheme } from "@src/theme/ThemeProvider";
 
 interface TextProps {
-  variant?: 'display1'
-  tag?: 'p' | 'li' | 'h1' | 'h2'| 'h3'| 'h4'| 'h5'| 'h6'| 'string'; 
+  variant?: ThemeTypographyVariants
+  tag?: 'p' | 'li' | 'h1' | 'h2'| 'h3'| 'h4'| 'h5'| 'h6'; 
   children?: React.ReactNode; 
   styleSheet?: StyleSheet; 
 }
 
 export default function Text({ styleSheet, variant, ...props}: TextProps) {
   
+  const theme = useTheme();
   const textVariant =  theme.typography.variants[variant];
   
   return (
