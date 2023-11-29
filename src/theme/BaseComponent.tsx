99465822eb3745d2eb3c0047662e84bc9545ecce
Arydiane <arydianejardim@gmail.com>
2023-11-28 22:00:43 -0300

    atualiza estilização BaseComponent

--- src/theme/BaseComponent.tsx
+++ src/theme/BaseComponent.tsx
@@ -8,6 +8,10 @@ interface StyledBaseComponent {
 }
 
 const StyledBaseComponent = styled.div<StyledBaseComponent>`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
+  flex-shrink: 0;
   ${({ styleSheet }) => parseStyleSheet(styleSheet)}
 `
 
