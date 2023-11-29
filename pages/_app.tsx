58ded3eb5cf02ad0e2f8d31f8a480504d82e5aa8
Arydiane <arydianejardim@gmail.com>
2023-11-28 21:13:07 -0300

    adiciona globalStyle

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import GlobalStyle from "@src/theme/GlobalStyle";
+
+export default 
+function App({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
