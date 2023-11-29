42df00ffafca7a91ae3c1dd7f8bb2c768fd0774d
Arydiane <arydianejardim@gmail.com>
2023-11-28 22:20:32 -0300

    adicionando themeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from "next/app";
 import GlobalStyle from "@src/theme/GlobalStyle";
+import ThemeProvider from "@src/theme/ThemeProvider";
 
 export default 
 function App({Component, pageProps}: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
