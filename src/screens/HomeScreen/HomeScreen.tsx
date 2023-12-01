5af92009faccff968e2798177b900e375b66f597
Arydiane <arydianejardim@gmail.com>
2023-12-01 20:43:15 -0300

    atualiza HomeScreen acrescenta menu

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,26 +1,25 @@
 import Box from "@src/components/Box/Box";
 import Background from "./patterns/Background/Background";
 import Menu from "./patterns/Menu/Menu";
-import Feed from "./patterns/Feed/Feed";
-import Text from "@src/components/Text/Text";
 import Footer from "./patterns/Footer/Footer";
 import { useTheme } from "@src/theme/ThemeProvider";
-import Link from "@src/components/Link/Link";
+
 
 export default function HomeScreen() {
-
-  const theme = useTheme()
+  const theme = useTheme();
 
   return (
-  <Box tag="main"
-    styleSheet={{ 
-      backgroundColor: theme.colors.neutral.x000, 
-      flex: 1, 
-      alignItems: 'center',
-    }}
-  >
-    <Background />
-    <Footer />
-  </Box>
+    <Box
+      tag="main"
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x000,
+        flex: 1,
+        alignItems: "center",
+      }}
+    >
+      <Background />
+      <Menu />
+      <Footer />
+    </Box>
   );
 }
