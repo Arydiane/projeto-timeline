386e344ca72c1d61a886e477c30fb23602b2d478
Arydiane <arydianejardim@gmail.com>
2023-12-01 20:16:21 -0300

    adiciona componente Footer

---
+++ src/screens/HomeScreen/patterns/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import Box from "@src/components/Box/Box";
+import Text from "@src/components/Text/Text";
+import { useTheme } from "@src/theme/ThemeProvider";
+
+export default function Footer() {
+  const theme = useTheme()
+  return (
+    <Box
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x900,
+        color: theme.colors.neutral.x000,
+        width: '100%',
+        height: '120px', 
+        paddingHorizontal: '24px',
+        paddingVertical: '24px',
+        alignItems: 'center', 
+        justifyContent: 'center',
+      }}
+    >
+      <Text variant="body2">
+        Feito com ❤️️ por Arydiane Jardim no curso da Alura
+      </Text>
+    </Box>
+  );
+}
