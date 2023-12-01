b4ee3f47152aa298f1d848b31eda87cdcb5f0579
Arydiane <arydianejardim@gmail.com>
2023-12-01 19:53:39 -0300

    adiciona componente Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,50 @@
+import ButtonBase, { ButtonBaseProps } from "./ButtonBase";
+import {ColorVariant, Variant, colorVariantBy} from './colorVariantBy'
+import { useTheme } from "@src/theme/ThemeProvider";
+import { ButtonSize, buttonSize } from "./buttonSize";
+
+interface ButtonProps extends ButtonBaseProps {
+  fullWidth?: boolean;
+  children: React.ReactNode;
+  colorVariant?: ColorVariant;
+  variant?: Variant;
+  size?: ButtonSize;
+}
+
+export default function Button({
+  children,
+  styleSheet,
+  fullWidth,
+  colorVariant, 
+  variant, 
+  size
+}: ButtonProps) {
+  const theme = useTheme(); 
+  return (
+    <ButtonBase
+      styleSheet={{
+        alignSelf: 'flex-start',
+        //Color Variant
+        ...colorVariantBy(theme, colorVariant, variant),
+        //Size
+        ...buttonSize[size], 
+        //FullWidth
+        ...(fullWidth && {
+          alignSelf: 'initial',
+        }),
+        ...styleSheet,
+      }}
+    >
+      {children}
+    </ButtonBase>
+  );
+}
+
+Button.defaultProps = {
+  fullWidth: false,
+  size: 'md',
+  variant: 'contained', 
+  colorVariant: 'positive',
+};
+
+Button.Base = ButtonBase;
