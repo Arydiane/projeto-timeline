af2bf4d29ca46a49d8abdbd1b4ec2e41da1ac94c
Arydiane <arydianejardim@gmail.com>
2023-11-30 21:16:17 -0300

    atualiza BaseCompoent

--- src/theme/BaseComponent.tsx
+++ src/theme/BaseComponent.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { StyleSheet } from "@src/theme/StyleSheet";
 import { parseStyleSheet } from '@displaykit/responsive_styles';
+import { type } from "os";
 
 interface StyledBaseComponent {
   styleSheet?: StyleSheet; 
+  ref: any;
 }
 
 const StyledBaseComponent = styled.div<StyledBaseComponent>`
@@ -14,12 +16,16 @@ const StyledBaseComponent = styled.div<StyledBaseComponent>`
   flex-shrink: 0;
   ${({ styleSheet }) => parseStyleSheet(styleSheet)}
 `
-
-export const BaseComponent = (props) => {
-  return (
-    <StyledBaseComponent {...props} />
-  )
+interface BaseComponentsProps {
+  styleSheet?: StyleSheet; 
+  [key: string]: any; 
 }
+
+export const BaseComponent = React.forwardRef<unknown, BaseComponentsProps>((props, ref) => {
+  return (
+    <StyledBaseComponent {...props} ref={ref} />
+  )
+})
 
 BaseComponent.defaultProps = {
   styleSheet: {},
