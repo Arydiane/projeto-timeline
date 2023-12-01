3257b985426a29bef1728b70a3f4fb0c87384269
Arydiane <arydianejardim@gmail.com>
2023-11-30 21:15:12 -0300

    atualiza componente Text

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,32 +2,37 @@ import { StyleSheet } from "@src/theme/StyleSheet";
 import { ThemeTypographyVariants } from "@src/theme/theme";
 import { BaseComponent } from "@src/theme/BaseComponent";
 import { useTheme } from "@src/theme/ThemeProvider";
+import React from "react";
 
 interface TextProps {
-  variant?: ThemeTypographyVariants
-  tag?: 'p' | 'li' | 'h1' | 'h2'| 'h3'| 'h4'| 'h5'| 'h6'; 
+  variant?: ThemeTypographyVariants;
+  tag?:  'p' | 'li' | 'h1' | 'h2'| 'h3'| 'h4'| 'h5'| 'h6' | string; 
   children?: React.ReactNode; 
   styleSheet?: StyleSheet; 
+  ref: any; 
 }
 
-export default function Text({ styleSheet, variant, ...props}: TextProps) {
-  
+const Text = React.forwardRef( ({ tag, styleSheet, variant, ...props}: TextProps, ref) => {
   const theme = useTheme();
   const textVariant =  theme.typography.variants[variant];
   
   return (
     <BaseComponent 
+      as={tag}
       styleSheet={{ 
         fontFamily: theme.typography.fontFamily, 
         ...textVariant,
         ...styleSheet,
       }} 
+      ref={ref}            
       {...props} 
     />
   )
-}
+})
+
+export default Text; 
 
 Text.defaultProps = {
   tag: 'p', 
-  variant: 'body2'
+  variant: 'body2',
 }
