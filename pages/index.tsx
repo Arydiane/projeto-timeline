61ffc7da515165f2bbfaf275d5faadb82d65463f
Arydiane <arydianejardim@gmail.com>
2023-12-04 18:47:34 -0300

    atualiza index adiciona função getStatisProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import { withTemplateConfig } from '@src/services/template/withTemplateConfig';
 
-export { default } from '@src/screens/HomeScreen/HomeScreen'
+export { default } from '@src/screens/HomeScreen/HomeScreen';
 
+export async function getStaticProps() {
+
+  return {
+    props: await withTemplateConfig({})
+  }
+}
