50ec1ce8b512247f1a5f431c5e8b926d46d32df9
Arydiane <arydianejardim@gmail.com>
2023-12-04 18:47:04 -0300

    adiciona serviço withTemplateConfig para ler o arquivo configuração yml

---
+++ src/services/template/withTemplateConfig.ts
@@ -0,0 +1,30 @@
+import readYamlFile from "read-yaml-file/index";
+import path from "path";
+
+interface TemplateConfig {
+  site?: {
+    title?: string;
+    description?: string;
+  }
+  personal?: {
+    name?: string; 
+    avatar?: string; 
+    socialNetworks?: {
+      youtube?: string; 
+      twitter?: string; 
+      instagram?: string; 
+      github?: string;
+      linkedin?: string;
+    }
+  }
+}
+
+export async function withTemplateConfig(props = {}) {
+  const PATH_TEMPLATE_CONFIG = path.resolve(".", "template-config.yml");
+  const templateConfig = await readYamlFile<TemplateConfig>(PATH_TEMPLATE_CONFIG);
+
+  return {
+    templateConfig,
+    ...props,
+  };
+}
