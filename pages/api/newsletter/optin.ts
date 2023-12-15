1147cde760d501606aa72bd4c4a5921bf0c6e14b
Arydiane <arydianejardim@gmail.com>
2023-12-14 22:18:22 -0300

    atualiza optin envia os dados para o supabase

--- pages/api/newsletter/optin.ts
+++ pages/api/newsletter/optin.ts
@@ -1,4 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest, NextApiResponse } from "next";
+
+//===== Supabase Setup =====
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const dbClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+//==========================
 
 const httpStatus = {
   Success: 200,
@@ -8,11 +15,38 @@ const httpStatus = {
 };
 
 const controlledByMethod = {
-  POST(req: NextApiRequest, res: NextApiResponse) {
+  async POST(req: NextApiRequest, res: NextApiResponse) {
+    const email = req.body.emailNewsletter;
+
+    //Validação
+    if (!Boolean(email) || !email.includes("@")) {
+      res
+        .status(httpStatus.BadRequest)
+        .json({ message: "Forneça um email válido. Exemplo: teste@teste.com" });
+      return;
+    }
+
+    //Cadastra o email na newsletter
+    const { error } = await dbClient
+      .from("newsletter_users")
+      .insert({ email: email, optin: true });
+    //Verifica erro
+    if (error) {
+      res.status(httpStatus.BadRequest).json({ message: "Erro no cadastro!" });
+      return;
+    }
+
+    //Cria um usuário no sistema
+    await dbClient.auth.admin.createUser({ email: email });
+
     res.status(httpStatus.Success).json({ message: "Post request!" });
   },
-  GET(req: NextApiRequest, res: NextApiResponse) {
-    res.status(httpStatus.Success).json({ message: "Get request!" });
+  async GET(req: NextApiRequest, res: NextApiResponse) {
+    const { data, error } = await dbClient.from("newsletter_users").select("*");
+
+    res
+      .status(httpStatus.Success)
+      .json({ message: "Get request!", total: data.length });
   },
 };
 
